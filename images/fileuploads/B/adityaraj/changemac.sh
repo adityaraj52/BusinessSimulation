1e5cfb56e701aa79e5c477854ec7ca68a7ed1933
Aditya Raj <aditya.raj@tu-clausthal.de>
2017-09-25 12:23:06 +0200

    changes

---
+++ images/fileuploads/B/adityaraj/changemac.sh
@@ -0,0 +1,10 @@
+sudo ifconfig en0 ether ec:1a:59:4b:33:05
+#sudo ifconfig en0 ether a4:d1:8c:d9:0f:c8
+#sudo ifconfig en3 ether a4:d1:8c:d9:0f:c8
+
+## MAC Addresses
+
+## Laptop               a4:d1:8c:d9:0f:c8
+## Apple USB adapter    00:f7:6f:6e:37:9c
+## iPhone               cc:25:ef:16:05:4b
+## Belkin router        ec:1a:59:4b:33:05
